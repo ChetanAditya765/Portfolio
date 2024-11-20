1257731b6712b0725acac3a374a5a62d2e3bd334
Chetan Aditya <142312111+ChetanAditya765@users.noreply.github.com>
2024-11-20 13:59:24 +0530

    Thumbnail

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Chetan Aditya',
     description:
       'Chetan Aditya is an aspiring software engineer who specializes in creating predictive models using Machine Learning',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://chetanaditya.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@CA_765',
   },
